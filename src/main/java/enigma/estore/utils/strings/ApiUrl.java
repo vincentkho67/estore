3199fd755967719ba7f4a5d0e77a36b4e0678040
Vincent <vincentkho67@gmail.com>
2024-08-20 12:33:31 +0700

    update api url

--- src/main/java/enigma/estore/utils/strings/ApiUrl.java
+++ src/main/java/enigma/estore/utils/strings/ApiUrl.java
@@ -2,5 +2,5 @@ package enigma.estore.utils.strings;
 
 public interface ApiUrl {
     public static final String BASE_URL = "/api/v1";
-    public static final String CATEGORIES = "categories";
+    public static final String CATEGORIES = "/categories";
 }
